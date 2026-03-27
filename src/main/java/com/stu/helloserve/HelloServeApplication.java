372b3598e667bef9d879282a0785a0c0b6563192
林胜 <15804790+mu-mu-yue-sheng@user.noreply.gitee.com>
2026-03-27 11:13:03 +0800

    服务端开发技术1,2

---
+++ src/main/java/com/stu/helloserve/HelloServeApplication.java
@@ -0,0 +1,13 @@
+package com.stu.helloserve;
+
+import org.springframework.boot.SpringApplication;
+import org.springframework.boot.autoconfigure.SpringBootApplication;
+
+@SpringBootApplication
+public class HelloServeApplication {
+
+    public static void main(String[] args) {
+        SpringApplication.run(HelloServeApplication.class, args);
+    }
+
+}
---
+++ src/test/java/com/stu/helloserve/HelloServeApplicationTests.java
@@ -0,0 +1,13 @@
+package com.stu.helloserve;
+
+import org.junit.jupiter.api.Test;
+import org.springframework.boot.test.context.SpringBootTest;
+
+@SpringBootTest
+class HelloServeApplicationTests {
+
+    @Test
+    void contextLoads() {
+    }
+
+}
